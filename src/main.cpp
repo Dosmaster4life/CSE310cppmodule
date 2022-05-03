36b9e4beb2b7f8153bc3218cb031ee744ff9c56f
dosmaster4life <jefferysuperman@gmail.com>
2022-05-03 13:01:22 -0600

    Created a function to get User Input

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include<fstream>
 using namespace std;
+
+int getUserInput() {
+    int amount = 0;
+    cout << "Type a number for how many words you want in your passphrase\n";
+    try {
+        cin >> amount;
+    }catch(exception e) {
+        cout << "Invalid input, type a number such as 5 or 10\n";
+    }
+    return amount;
+}
+
 int main() {
-cout << "Hello World";
+getUserInput();
+
 return 0;
 }
