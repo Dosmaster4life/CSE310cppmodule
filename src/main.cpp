49331771af294ea5f2339cb4ec78e1c52ae84a39
dosmaster4life <jefferysuperman@gmail.com>
2022-05-04 16:49:01 -0600

    Added read file functionality and display, minor bugs remain

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include<fstream>
+#include <vector>
+#include <stdlib.h>   
 using namespace std;
 
 int getUserInput() {
@@ -13,8 +15,40 @@ int getUserInput() {
     return amount;
 }
 
+vector<string> getWordsFromFile(int amount) {
+    int lineCount = 0;
+    string currentWord;
+    vector<string> listOfWords;
+    vector<string> wordList;
+    
+    ifstream filePath("words_alpha.txt");
+    while(getline(filePath,currentWord)) {
+        wordList.push_back(currentWord);
+        lineCount += 1;
+    
+    }
+    for(int i = 0; i < amount; i++) {
+        listOfWords.push_back(wordList[rand() % lineCount]);
+    }
+
+
+    return listOfWords;
+}
+
 int main() {
-getUserInput();
+ vector<string> result = getWordsFromFile(getUserInput());
+    string combinedWords = "";
+ for(int i = 0; i < result.size(); i++) {
+    combinedWords += result[i] +  " ";
+    cout << result[i] << endl;
+    cin.get();
+    cin.get();
+
+ }
+cout << combinedWords << endl;
+ 
+ 
+
 
 return 0;
 }
