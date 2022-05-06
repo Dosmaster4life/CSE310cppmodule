684128e21fe2221d2da2d5823f279e5e61e410c2
Dosmaster4life <jefferysuperman@gmail.com>
2022-05-06 12:57:35 -0600

    final updates to the files

--- README.md
+++ README.md
@@ -1,30 +1,38 @@
 # Overview
 
-{Important!  Do not say in this section that this is college assignment.  Talk about what you are trying to accomplish as a software engineer to further your learning.}
+{PassPhrase is a program that generates pass phrases and saves them for the user to view later. }
 
-{Provide a description the software that you wrote to demonstrate the C++ language.}
+{PassPhrase does the following, 
+1. Prompts the user how many words they want in a pass phrase
+2. Searches a file and selects random pass phrases
+3. Displays the pass phrase
+4. Prompts the user to ask what filename they want.
+5. Saves the passphrase to a file
+6. Repeats if the user wants to create more pass phrases.
+}
 
-{Describe your purpose for writing this software.}
+{This software was developed to help users create unique pass phrases. Often hardware wallets for crypto currency have pass phrases as a backup if the user forgot their password. PassPhrase will create unique pass phrases based on how many words the user wants in their phrase.}
 
 {Provide a link to your YouTube demonstration.  It should be a 4-5 minute demo of the software running and a walkthrough of the code.  Focus should be on sharing what you learned about the language syntax.}
 
-[Software Demo Video](http://youtube.link.goes.here)
+[Software Demo Video](https://youtu.be/GR01rjbUmFI)
 
 # Development Environment
 
-{Describe the tools that you used to develop the software}
+{I developed this in Visual Studio Code and Codeblocks}
 
-{Describe the programming language that you used and any libraries.}
+I used C++ with (iostream, fstream, vector, stdlib.h, Time.h)
 
 # Useful Websites
 
 {Make a list of websites that you found helpful in this project}
-* [Web Site Name](http://url.link.goes.here)
-* [Web Site Name](http://url.link.goes.here)
+* [W3 Schools](https://www.w3schools.com/cpp/cpp_class_methods.asp)
+* [cpluscplus.com](https://www.cplusplus.com/doc/tutorial/files/)
 
 # Future Work
 
 {Make a list of things that you need to fix, improve, and add in the future.}
-* Item 1
-* Item 2
-* Item 3
+* Pass Phrases are in an encrypted file
+* Pass Phrases are protected by a password
+* A graphical user interface
+* A better random function
--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ class PassPhraseCreation
 {
 
 public:
-    string promptFilePath()
+    string promptFilePath() // returns the name the user wants for the text file
     {
         cout << "Type a filename for this passphrase to be saved.";
         cout << endl;
@@ -17,7 +17,7 @@ public:
         cin >> filename;
         return filename;
     }
-    bool writeAnother()
+    bool writeAnother() // returns whether the user wants to generate and write another passphrase to a file
     {
         string writeAnother = "";
         cout << "Would you like to generate another Passphrase, y for yes or anything else to exit)\n";
@@ -31,13 +31,13 @@ public:
             return false;
         }
     }
-    int getUserInput()
+    int getUserInput() // Asks the user how many words they want in the passphrase
     {
         int amount = 0;
 
         cout << "Type a number for how many words you want in your passphrase\n";
         ;
-        try
+        try // catches errors if the user types a number or an invalid charactar
         {
             cin >> amount;
         }
@@ -49,30 +49,32 @@ public:
     }
     void writeStringToFile(string fileContents, string filename)
     {
-        ofstream outputFile;
-        outputFile.open(filename + ".txt");
-        outputFile << fileContents;
-        outputFile.close();
+        ofstream outputFile; // used for writing to files
+        outputFile.open(filename + ".txt"); // opens a new file with the users desired file name
+        outputFile << fileContents; // writes to the file
+        outputFile.close(); // closes the file
     }
-    string getWordsFromFile(int amount)
+    string getWordsFromFile(int amount) // reads the passphrases and chooses random words to return as one string
     {
         int lineCount = 0;
         string currentWord;
         string passPhrase = "";
-        vector<string> wordList;
+        vector<string> wordList; // a vector that contains every word in the word list
 
-        fstream filePath("words_alpha.txt");
-        while (getline(filePath, currentWord))
+        fstream filePath("words_alpha.txt"); // opens the file containing every passphrase word
+        while (getline(filePath, currentWord)) // reads each word line by line
         {
-            wordList.push_back(currentWord);
-            lineCount += 1;
+            wordList.push_back(currentWord); // adds each word to the vector
+            lineCount += 1; // counts how many lines are in the file
             // cout << currentWord << endl;
         }
-        filePath.close();
+
+        filePath.close(); // closes the file
+         srand(time(0)); // used to randomize based on time
         for (int i = 0; i < amount; i++)
         {
-            int rn = rand() % lineCount - 1;
-            passPhrase.append(wordList[rn] + ' ');
+            string randomString = wordList[rand()%lineCount] + ' ';
+            passPhrase.append(randomString); // The random word is added to the final passphrase
         }
 
         return passPhrase;
@@ -81,26 +83,26 @@ public:
 
 int main()
 {
-    bool isRunning = true;
-    PassPhraseCreation prompt;
+    bool isRunning = true; // while true the loop will allow more passphrases to be created
+    PassPhraseCreation prompt; // class containing each method neccessary to run the program
     do
     {
-        srand(time(0)); // used to randomize
-        std::cout.flush();
-        string result = prompt.getWordsFromFile(prompt.getUserInput());
-        cout << result << endl;
+
+        std::cout.flush(); // used to help display output on m1 mac
+        string result = prompt.getWordsFromFile(prompt.getUserInput()); // gets the passphrase
+        cout << result << endl; // displays the passphrase
 
         cout << endl;
 
-        string filename = prompt.promptFilePath();
+        string filename = prompt.promptFilePath(); // Gets the desired filename from the user
         cout << endl;
-        prompt.writeStringToFile(result, filename);
-        cout << "File written to successfully.";
-        std::cout.flush();
+        prompt.writeStringToFile(result, filename); // writes the passphrase to a file
+        cout << "File written to successfully.\n"; // Lets the user know the operation was completed
+        std::cout.flush(); // helps with display output on m1 mac
 
-        isRunning = prompt.writeAnother();
+        isRunning = prompt.writeAnother(); // checks if the user wants to create more passphrases.
 
-    } while (isRunning);
+    } while (isRunning); // while the user wants to create more passphrases, loop
 
     return 0;
 }
